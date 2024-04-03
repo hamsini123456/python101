855eb90135b024f4d17557cb5f1d1542e096e8d8
hamsini123456 k <av.en.u4aie22162@av.students.amrita.edu>
2024-04-03 13:17:44 +0000

    variables in python

--- var.py
+++ var.py
@@ -1,1 +1,56 @@
-print ("hamsini")
+# this file explains variables in python
+
+print ('My Blog is AIFOSS')
+
+print ('2+3')
+
+# + addition
+# - Subtraction
+# /(float division) 
+# //(integer division)
+# * multiply
+# % modulo - remainder
+
+print(4//2)
+
+# type is a function to return the type of variable
+number = 1 #int
+print(type(number))
+
+number = float(number) #float
+print(type(number))
+
+number = int(number) #int
+print(type(number))
+#print(type(1.1))
+
+
+# find the syntax for floor and ceil equivalent functions in c
+# import math
+
+# num = 4.9
+# floored_num = math.floor(num)
+# print(floored_num)  
+# import math
+
+# num = 4.1
+# ceiled_num = math.ceil(num)
+# print(ceiled_num)  
+#in c
+#include <stdio.h>
+#include <math.h>
+
+# int main() {
+#     double number = 5.67;
+#     double result_floor = floor(number);
+#     double result_ceil = ceil(number);
+    
+#     printf("Original number: %f\n", number);
+#     printf("Floor value: %f\n", result_floor);
+#     printf("Ceil value: %f\n", result_ceil);
+    
+#     return 0;
+# }
+
+
+
